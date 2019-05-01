9e3276d5de3a6f0604fa6c2a29ea12d014ce5aab
David Judd <david.judd@gmail.com>
2019-05-01 08:31:59 -0400

    Added test with mock repository

--- test/Hoopp.Flash.Trades.InteractionTests/Controllers/TradeControllerTests.cs
+++ test/Hoopp.Flash.Trades.InteractionTests/Controllers/TradeControllerTests.cs
@@ -1,7 +1,11 @@
+using System;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
+using FakeItEasy;
 using Hoopp.Flash.Core.Test;
 using Hoopp.Flash.Trades.Domain.Configuration;
+using Hoopp.Flash.Trades.Domain.Models;
 using Hoopp.Flash.Trades.Web;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
@@ -19,6 +23,21 @@ namespace Hoopp.Flash.Trades.InteractionTests.Controllers
             _factory = factory;
         }
 
+        private HttpClient CreateClientWithDisabledConfig()
+        {
+            return _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.ReplaceAsSingleton<IOptions<TradeServiceOptions>>(
+                        x => Options.Create(new TradeServiceOptions
+                        {
+                            ConfigEndpointEnabled = false
+                        }));
+                });
+            }).CreateClient();
+        }  
+
         [Fact]
         public async Task Options_trades_returns_config_200()
         {
@@ -33,7 +52,7 @@ namespace Hoopp.Flash.Trades.InteractionTests.Controllers
         }
 
         [Fact]
-        public async Task Options_trades_with_custom_config_returns_200()
+        public async Task Options_trades_with_custom_config_returns_config_200()
         {
             // NOTE: Shows how to replace configuration options, but any registered
             // service can be replaced using this method
@@ -69,22 +88,69 @@ namespace Hoopp.Flash.Trades.InteractionTests.Controllers
         [Fact]
         public async Task Options_trades_with_disabled_config_returns_404()
         {
-            // NOTE: Shows how to replace configuration options, but any registered
-            // service can be replaced using this method
+            using (var client = CreateClientWithDisabledConfig())
+            {
+                var response = await client.OptionsAsync("api/trades");
+                Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+            }
+        }
+
+        [Fact]
+        public async Task Get_trades_with_invalid_id_returns_400()
+        {
+            // NOTE: Shows how to replace a service with a mock service
             using (var client = _factory.WithWebHostBuilder(builder =>
             {
                 builder.ConfigureTestServices(services =>
                 {
-                    services.ReplaceAsSingleton<IOptions<TradeServiceOptions>>(
-                        x => Options.Create(new TradeServiceOptions
+                    services.ReplaceAsSingleton<ITradeRepository>(
+                        x =>
                         {
-                            ConfigEndpointEnabled = false
-                        }));
+                            var mock = A.Fake<ITradeRepository>();
+                            A.CallTo(() => mock.GetByIdAsync("foo")).Returns(Task.FromResult<Trade>(null));
+                            return mock;
+                        });
                 });
             }).CreateClient())
             {
-                var response = await client.OptionsAsync("api/trades");
-                Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+                var response = await client.GetAsync($"api/trades/foo");
+                Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            }
+        }
+
+        [Fact]
+        public async Task Get_trades_with_valid_id_returns_trade_200()
+        {
+            // NOTE: Shows how to replace a service with a mock service
+            using (var client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.ReplaceAsSingleton<ITradeRepository>(
+                        x =>
+                        {
+                            var trade = new Trade
+                            {
+                                Id = "1",
+                                CreatedAt = DateTimeOffset.Now,
+                                Notes = "notes",
+                                Ticker = "ticker"
+                            };
+                            var mock = A.Fake<ITradeRepository>();
+                            A.CallTo(() => mock.GetByIdAsync("1")).Returns(Task.FromResult(trade));
+                            return mock;
+                        });
+                });
+            }).CreateClient())
+            {
+                var response = await client.GetAsync($"api/trades/1");
+                Assert.True(response.IsSuccessStatusCode);
+
+                var result = await response.ReadAsAsync<Trade>();
+                Assert.NotNull(result);
+                Assert.Equal("1", result.Id);
+                Assert.Equal("notes", result.Notes);
+                Assert.Equal("ticker", result.Ticker);
             }
         }
     }
