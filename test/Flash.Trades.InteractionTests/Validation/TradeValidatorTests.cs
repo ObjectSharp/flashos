5ae19637bb4192d0dc20ec2336cde8036eeece7b
David Judd <david.judd@gmail.com>
2019-05-02 08:55:21 -0400

    Validator tests

--- test/Flash.Trades.InteractionTests/Validation/TradeValidatorTests.cs
+++ test/Flash.Trades.InteractionTests/Validation/TradeValidatorTests.cs
@@ -1,7 +1,36 @@
+using Flash.Trades.Domain.Models;
+using Flash.Trades.Domain.Validation;
+using FluentValidation.TestHelper;
+using Xunit;
+
 namespace Flash.Trades.UnitTests.Validators
 {
     public class TradeValidatorTests
     {
-        // TIP: https://fluentvalidation.net/testing
+        private readonly TradeValidator _validator;
+
+        public TradeValidatorTests()
+        {
+            _validator = new TradeValidator();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("\t\n")]
+        [InlineData("AAA")]
+        public void Trade_ticker_validation_should_return_error(string ticker)
+        {
+            var trade = new Trade { Ticker = ticker };
+            _validator.ShouldHaveValidationErrorFor(x => x.Ticker, trade);
+        }
+
+        [Theory]
+        [InlineData("APPL")]
+        public void Trade_ticker_validation_should_not_return_error(string ticker)
+        {
+            var trade = new Trade { Ticker = ticker };
+            _validator.ShouldNotHaveValidationErrorFor(x => x.Ticker, trade);
+        }
     }
 }
